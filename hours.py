cc0641ee1c58dca98bdebe07247b9997035f8bf7
Chuck Grimmett <cagrimmett@gmail.com>
2016-04-02 15:57:56 -0700

    AM vs PM breakdown and pie chart

---
+++ hours.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+import os
+import csv
+from collections import Counter
+import datetime
+from matplotlib import pyplot as plt
+
+location = os.getcwd()
+
+with open(location + '/photo_dates_location.csv') as f:
+    reader = csv.reader(f, delimiter=',', quotechar='"')
+    reader.next() #skip header
+    time_csv = [row[2] for row in reader]
+
+ampm_split = [i.split(' ')[1] for i in time_csv]    
+
+#print ampm_split
+
+count = Counter(time_csv)
+
+print "PM: ", ampm_split.count("PM"), "AM: ", ampm_split.count("AM")
+
+#pm_total = sum(v for k, v in [(x, month_name.count(x)) for x in set(spring)])
+
+
+######## Pie chart ########
+count = [ampm_split.count("PM"),ampm_split.count("AM")]
+labels = ['PM','AM']
+colors = ['#90DBFD', '#E36A1E']
+plt.pie(count, colors=colors, labels=labels, autopct='%1.1f%%')
+plt.axis('equal')
+plt.suptitle("AM vs PM photos", fontsize=20)
+#plt.show()
+plt.savefig('img/am_pm_pie.png')
